4fc232dea1f3fd11bf8bd21a71d24c4577cc9252
Mahidhar <mahidhar.c@gmail.com>
2014-08-18 15:33:58 +0530

    Solutions for 1 and 2

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -5,3 +5,14 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;Problem 1
+(apply + (set (concat (range 0 1000 3) (range 0 1000 5))))
+
+;Problem 2
+(def fib-seq
+  (lazy-cat [1 2] (map + (rest fib-seq) fib-seq)))
+
+(apply + (filter even? (take 32 fib-seq)))
+
+;Problem 3
