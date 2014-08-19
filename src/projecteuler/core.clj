4c5238dd080bbd016a5a17e964f549eeeaca4aea
Mahidhar <mahidhar.c@gmail.com>
2014-08-19 22:23:12 +0530

    Problem 3 draft

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -11,8 +11,17 @@
 
 ;Problem 2
 (def fib-seq
-     (lazy-cat [1 2] (map + (rest fib-seq) fib-seq)))
+     (concat [1 2] (map + (rest fib-seq) fib-seq)))
 
 (apply + (filter even? (take 32 fib-seq)))
 
 ;Problem 3
+(defn factor? [num div]
+  (if (zero? (rem num div)) true false))
+
+(empty? (filter true? (map zero? '(1 3 2 4))))
+
+(defn prime? [num]
+  (empty? (filter true? (map zero? (map rem num (range 2 (Math/sqrt num)))))))
+
+(prime? 17)
